89dcf64a0d3d344f25d27107e85e62410f72db59
Dikayyy <adikaakbarlove@gmail.com>
2025-07-09 12:52:08 +0700

    feat(record): search field

---
+++ lib/core/widgets/custom_text_field.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:iampelgading/core/colors/app_colors.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final String? hintText;
+  final TextEditingController? controller;
+  final TextInputType keyboardType;
+  final bool obscureText;
+  final String? Function(String?)? validator;
+  final void Function(String)? onChanged;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final int? maxLines;
+  final int? minLines;
+  final bool readOnly;
+  final VoidCallback? onTap;
+  final EdgeInsetsGeometry? contentPadding;
+  final FocusNode? focusNode;
+  final bool autofocus;
+  final bool enabled;
+
+  const CustomTextField({
+    super.key,
+    required this.label,
+    this.hintText,
+    this.controller,
+    this.keyboardType = TextInputType.text,
+    this.obscureText = false,
+    this.validator,
+    this.onChanged,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.maxLines = 1,
+    this.minLines,
+    this.readOnly = false,
+    this.onTap,
+    this.contentPadding,
+    this.focusNode,
+    this.autofocus = false,
+    this.enabled = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Label
+        Text(
+          label,
+          style: const TextStyle(
+            color: Color(0xFF343434),
+            fontSize: 16,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+
+        const SizedBox(height: 4),
+
+        // Text Field
+        TextFormField(
+          controller: controller,
+          keyboardType: keyboardType,
+          obscureText: obscureText,
+          validator: validator,
+          onChanged: onChanged,
+          maxLines: maxLines,
+          minLines: minLines,
+          readOnly: readOnly,
+          onTap: onTap,
+          focusNode: focusNode,
+          autofocus: autofocus,
+          enabled: enabled,
+          style: TextStyle(
+            color: AppColors.neutral[200],
+            fontSize: 16,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w500,
+          ),
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: TextStyle(
+              color: AppColors.neutral[50],
+              fontSize: 16,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w500,
+            ),
+            prefixIcon: prefixIcon,
+            suffixIcon: suffixIcon,
+            contentPadding: contentPadding ?? const EdgeInsets.all(14),
+            filled: true,
+            fillColor: Colors.white,
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 1, color: AppColors.base),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 1.5, color: AppColors.base),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 1, color: AppColors.error[400]!),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 1.5, color: AppColors.error[400]!),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 1, color: AppColors.neutral[50]!),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
